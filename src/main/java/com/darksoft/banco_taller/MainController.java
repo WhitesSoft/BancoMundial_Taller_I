769b6be8b8b1cef31e05f086fdb2f175e15ca665
Juanchis <juan.moonteroo@gmail.com>
2022-06-21 21:39:02 -0400

    Error al subir datos en socios

--- src/main/java/com/darksoft/banco_taller/MainController.java
+++ src/main/java/com/darksoft/banco_taller/MainController.java
@@ -4,14 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 import java.net.URL;
--- src/main/java/com/darksoft/banco_taller/SociosController.java
+++ src/main/java/com/darksoft/banco_taller/SociosController.java
@@ -1,4 +1,84 @@
 package com.darksoft.banco_taller;
 
-public class SociosController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpResponse;
+import java.util.ResourceBundle;
+
+public class SociosController implements Initializable {
+
+    @FXML
+    private Button btnAgregarSocio;
+
+    @FXML
+    private TextField btnCuenta;
+
+    @FXML
+    private ComboBox<?> cbBanco;
+
+    @FXML
+    private ComboBox<?> cbCiudad;
+
+    @FXML
+    private ComboBox<?> cbMoneda;
+
+    @FXML
+    private TextField fieldApellido;
+
+    @FXML
+    private TextField fieldCarnet;
+
+    @FXML
+    private TextField fieldNombre;
+
+    @FXML
+    private ListView<?> lvSocios;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    private void agregarSocio() throws IOException {
+        //Obtenemos los datos a enviar
+        JSONObject nuevoSocio = new JSONObject();
+        nuevoSocio.put("nombre_socio", fieldNombre.getText());
+        nuevoSocio.put("apellido_socio", fieldApellido.getText());
+        nuevoSocio.put("cedula", fieldCarnet.getText());
+
+        //Hacemos el post
+        HttpClient cliente = HttpClient.newHttpClient();
+        HttpRequest solicitud = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/socios"))
+                .header("Content-Type", "application/json")
+                .POST(BodyPublishers.ofString(nuevoSocio.toString()))
+                .build();
+
+        String respuesta = cliente.sendAsync(solicitud, BodyHandlers.ofString())
+                .thenApply(HttpResponse::body).join();
+
+        System.out.println(respuesta);
+        if (respuesta.equals(""))
+            System.out.println("Socio agregado");
+        else{
+            System.out.println("No se pudo agregar el socio");
+        }
+
+    }
 }
